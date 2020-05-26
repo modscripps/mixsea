2a15cc449882b69e133d9029909f4a8e3d626925
Jesse Cusack <jmcusack@ucsd.edu>
2020-05-26 09:02:15 -0400

    add test for const s in overturns

--- mixsea/tests/test_overturn.py
+++ mixsea/tests/test_overturn.py
@@ -13,3 +13,14 @@ def test_overturn(ctd_profile):
         ctd_profile["lat"][0],
     )
     assert np.nanmean(eps) < 1e-4
+
+    
+def test_overturn_const_s(ctd_profile):
+    eps, n2 = overturn.nan_eps_overturn(
+        ctd_profile["p"],
+        ctd_profile["t"],
+        35,
+        ctd_profile["lon"][0],
+        ctd_profile["lat"][0],
+    )
+    assert np.nanmean(eps) < 1e-4
