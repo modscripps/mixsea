240e87958a47464f73da6168b3b10f4ea72656c1
Gunnar Voet <gvoet@ucsd.edu>
2020-05-28 15:20:52 -0700

    Change test data names and improve docs.
    - Change header in test data files to reflect new naming convention.
    - Change tests, doc notebooks to run with new names in test data.
    - Clean up docs.

--- mixsea/tests/test_overturn.py
+++ mixsea/tests/test_overturn.py
@@ -6,9 +6,9 @@ from mixsea import overturn
 # Use the ctd profile defined as fixture in conftest.py
 def test_overturn(ctd_profile):
     eps, n2 = overturn.nan_eps_overturn(
-        ctd_profile["p"],
+        ctd_profile["depth"],
         ctd_profile["t"],
-        ctd_profile["s"],
+        ctd_profile["SP"],
         ctd_profile["lon"][0],
         ctd_profile["lat"][0],
     )
@@ -17,7 +17,7 @@ def test_overturn(ctd_profile):
 
 def test_overturn_const_s(ctd_profile):
     eps, n2 = overturn.nan_eps_overturn(
-        ctd_profile["p"],
+        ctd_profile["depth"],
         ctd_profile["t"],
         35,
         ctd_profile["lon"][0],
