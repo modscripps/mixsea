Fixed major indexing bug in strain variance calc
There are steps in which the shear/strain spectra arrays are sub-sampled:
1) using `m_include_sh` or `m_include_st` to zoom in on the spectral window specified by the user
2) using `iimsh` or `iimst` to cutoff high wavenumbers.

Previously, the `iim*` erroneously refered to the indices of arrays that were already indexed by `m_include_*` and yet were later erroneously used to index the full arrays (e.g. `m[iimsh]`). This is typically not a problem for shear because the first index is usually 0 (the window scale) and thus `iimsh` is equal to `m_include_sh[iimsh]`. However, for strain the first index is typically strictly greater than 0, so `iimst` is almost never equal to `m_include_st[iimst]`.

Essentially what this means is that strain variance was typically being integrated over a spectral window of much larger scales than specified by the user, and was thus giving erroneous results (I am not exactly sure the theoretical reason that strain is meant to be evaluated over higher wave numbers than shear, but Kunze recommends a min wavenumber that corresponds to a max wavelength of 150 m. In the documentation example, this was erroneously being set to 300 m by this bug). This bug was not as bad as it could have been because it was applied to both the GM and observed spectra, and so the errors cancelled out to some extent.

mixsea/shearstrain.py
@@ -298,7 +298,7 @@ def gm_shear_variance(m, iim, N):
     N0 = 5.24e-3  # reference buoyancy frequency = 3 cph
     b = 1300  # thermocline scale depth
     jstar = 3
-    E0 = 6.3e-5  # GM energy level
+    E0 = 6.3e-5  # GM76 energy level
     Pgm = (
         (3 * np.pi * E0 * b * jstar / 2)
         * m ** 2
@@ -699,11 +699,13 @@ def shearstrain(
             iimsh, Mmax_sh[iwin] = find_cutoff_wavenumber(
                 Ptot_sh[m_include_sh], m[m_include_sh], sh_integration_limit
             )
+            iimsh = m_include_sh[iimsh]
 
         # Strain cutoff wavenumber
         iimst, Mmax_st[iwin] = find_cutoff_wavenumber(
             Ptot_st[m_include_st], m[m_include_st], st_integration_limit
         )
+        iimst = m_include_st[iimst]
 
         if calcsh:
             # Integrate shear spectrum to obtain shear variance